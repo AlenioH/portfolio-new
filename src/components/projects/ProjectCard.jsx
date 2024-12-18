07a3ae694689336cb148629fa52883320b549fb3
Alena Hasslacher <alena.hasslacher@gmail.com>
2024-12-18 13:43:28 +0100

    Add current projects, layout tweaks for projects page

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import {
-  Button, Card, Badge, Col,
+  Button, Card, Badge,
 } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { ThemeContext } from 'styled-components';
@@ -16,6 +16,10 @@ const styles = {
   },
   cardStyle: {
     borderRadius: 10,
+    height: '100%',
+    display: 'grid',
+    gridTemplateRows: '300px 25% 3.5rem auto',
+    gap: '0.5rem',
   },
   cardTitleStyle: {
     fontSize: 24,
@@ -31,6 +35,11 @@ const styles = {
   buttonStyle: {
     margin: 5,
   },
+  imgStyle: {
+    height: '300px',
+    aspectRatio: '1/1',
+    objectFit: 'cover',
+  },
 };
 
 const ProjectCard = (props) => {
@@ -40,52 +49,55 @@ const ProjectCard = (props) => {
   const { project } = props;
 
   return (
-    <Col>
-      <Card
-        style={{
-          ...styles.cardStyle,
-          backgroundColor: theme.cardBackground,
-          borderColor: theme.cardBorderColor,
-        }}
-        text={theme.bsSecondaryVariant}
-      >
-        <Card.Img variant="top" src={project?.image} />
-        <Card.Body>
-          <Card.Title style={styles.cardTitleStyle}>{project.title}</Card.Title>
-          <Card.Text style={styles.cardTextStyle}>
-            {parseBodyText(project.bodyText)}
-          </Card.Text>
-        </Card.Body>
+    <Card
+      style={{
+        ...styles.cardStyle,
+        backgroundColor: theme.cardBackground,
+        borderColor: theme.cardBorderColor,
+      }}
+      text={theme.bsSecondaryVariant}
+    >
+      <Card.Img variant="top" src={project?.image} style={styles.imgStyle} />
+      <Card.Body>
+        <Card.Title style={styles.cardTitleStyle}>{project.title}</Card.Title>
+        <Card.Text style={styles.cardTextStyle}>
+          {parseBodyText(project.bodyText)}
+        </Card.Text>
+      </Card.Body>
 
-        <Card.Body>
-          {project?.links?.map((link) => (
-            <Button
-              key={link.href}
-              style={styles.buttonStyle}
-              variant={'outline-' + theme.bsSecondaryVariant}
-              onClick={() => window.open(link.href, '_blank')}
+      <Card.Body>
+        {project?.links?.map((link) => (
+          <Button
+            key={link.href}
+            style={styles.buttonStyle}
+            variant={'outline-' + theme.bsSecondaryVariant}
+            onClick={() => window.open(link.href, '_blank')}
+          >
+            {link.text}
+          </Button>
+        ))}
+      </Card.Body>
+      {project.tags && (
+        <Card.Footer style={{
+          backgroundColor: theme.cardFooterBackground,
+          height: 'max-content',
+          alignSelf: 'end',
+        }}
+        >
+          {project.tags.map((tag) => (
+            <Badge
+              key={tag}
+              pill
+              bg={theme.bsSecondaryVariant}
+              text={theme.bsPrimaryVariant}
+              style={styles.badgeStyle}
             >
-              {link.text}
-            </Button>
+              {tag}
+            </Badge>
           ))}
-        </Card.Body>
-        {project.tags && (
-          <Card.Footer style={{ backgroundColor: theme.cardFooterBackground }}>
-            {project.tags.map((tag) => (
-              <Badge
-                key={tag}
-                pill
-                bg={theme.bsSecondaryVariant}
-                text={theme.bsPrimaryVariant}
-                style={styles.badgeStyle}
-              >
-                {tag}
-              </Badge>
-            ))}
-          </Card.Footer>
-        )}
-      </Card>
-    </Col>
+        </Card.Footer>
+      )}
+    </Card>
   );
 };
 
